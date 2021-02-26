64af0fd1238dfd1981ff5ffff93b2e067b5d4e01
Minho Lee <lkjjr0424@hanmail.net>
2021-02-26 19:27:41 +0900

    jwt token check

--- react_app/src/components/Main.js
+++ react_app/src/components/Main.js
@@ -1,4 +1,18 @@
 import React from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
-export default function Main() {}
+export default function Main() {
+  const api_url = "http://localhost:5000/";
+  const token = localStorage.getItem("token");
+  axios.get(api_url + "protected", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  );
+}
